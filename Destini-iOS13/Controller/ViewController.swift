27412f8e462261955309b4230f3fa6372ab021db
gaoey <gaoey@hotmail.com>
2020-04-11 14:43:20 +0700

    initialize and update as README

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+    var story = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let current = story.getCurrentStory()
+        if(userAnswer == current.ch1){
+            story.nextQuestion(goTo: current.dest1)
+        }else{
+            story.nextQuestion(goTo: current.dest2)
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        updateUI()
+    }
+    
+    @objc func updateUI(){
+        let current = story.getCurrentStory()
+        storyLabel.text = current.question
+        choice1Button.setTitle(current.ch1, for: .normal)
+        choice2Button.setTitle(current.ch2, for: .normal)
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+
+struct Story {
+    let question: String
+    let ch1: String
+    let ch2: String
+    let dest1: Int
+    let dest2: Int
+    
+    init(title: String, choice1: String,  choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        question = title
+        ch1 = choice1
+        ch2 = choice2
+        dest1 = choice1Destination
+        dest2 = choice2Destination
+    }
+}
